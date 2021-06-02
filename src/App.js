cefb3670e28b4f2f9bb7124ae42c09d89447cb5b
Likhan Sarkar <likhansarkar007@gmail.com>
2021-06-02 14:30:19 +0530

    initial Commit

--- src/App.js
+++ src/App.js
@@ -9,8 +9,6 @@ function App() {
 
   const authDataHandler = () => {
     setAuthData(true);
-    // console.log("app.js");
-    // console.log(authData);
   };
 
   setCookie("Cookie", "__cfduid=df9b865983bd04a5de2cf5017994bbbc71618565720", {
